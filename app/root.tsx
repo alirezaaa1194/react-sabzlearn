05a771b42816b75626ea4489b7a42f8f4309442a
Alireza <alirezaaa1194@gmail.com>
2025-02-09 16:57:28 +0330

    add 404 page

--- app/root.tsx
+++ app/root.tsx
@@ -3,6 +3,7 @@ import { isRouteErrorResponse, Links, Meta, Outlet, Scripts, ScrollRestoration, 
 import type { Route } from "./+types/root";
 import stylesheet from "./app.css?url";
 import session from "./sessions.server";
+import NotFound from "./routes/NotFound";
 
 export const links: Route.LinksFunction = () => [{ rel: "stylesheet", href: stylesheet }];
 
@@ -35,7 +36,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-
 export default function App() {
   return <Outlet />;
 }
@@ -54,15 +54,16 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   }
 
   return (
-    <main className="pt-16 p-4 container mx-auto">
-      <h1>{message}</h1>
-      <p>{details}</p>
-      {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
-          <code>{stack}</code>
-        </pre>
-      )}
-      
-    </main>
+    <NotFound />
+    // <main className="pt-16 p-4 container mx-auto">
+    //   <h1>{message}</h1>
+    //   <p>{details}</p>
+    //   {stack && (
+    //     <pre className="w-full p-4 overflow-x-auto">
+    //       <code>{stack}</code>
+    //     </pre>
+    //   )}
+
+    // </main>
   );
 }
