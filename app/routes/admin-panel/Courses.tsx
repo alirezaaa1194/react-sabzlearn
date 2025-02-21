ac18f8e0083878f16523565f9dfa87843f5ea7d4
Alireza <alirezaaa1194@gmail.com>
2025-02-21 15:26:17 +0330

    change heroui version to 6.1.3

--- app/routes/admin-panel/Courses.tsx
+++ app/routes/admin-panel/Courses.tsx
@@ -3,12 +3,15 @@ import CoursesList from "~/components/admin-panel/Courses/CoursesList";
 import type { Route } from "./+types/Courses";
 import { getAllCategories, getAllCourses, getCookie, saveNewCourse } from "~/utils/utils";
 import type { MetaFunction } from "react-router";
+import type { categoryType } from "~/types/category.type";
 
 export async function loader({ params, request }: Route.LoaderArgs) {
-  const allCategories = await getAllCategories();
+  let allCategories = await getAllCategories();
   const allCourses = await getAllCourses();
 
-  return { allCategories, allCourses };
+  const sortedCategories = [...allCategories.data].sort((a: categoryType, b: categoryType) => (new Date(b.createdAt) as any) - (new Date(a.createdAt) as any)) as any
+
+  return { sortedCategories, allCourses };
 }
 
 export const meta: MetaFunction = () => {
@@ -22,7 +25,7 @@ function Courses({ loaderData }: Route.ComponentProps) {
         <span className="text-xl font-lalezar text-white">
           افزودن <span className="text-secondary">دوره</span> جدید
         </span>
-        <AddCourseForm categories={loaderData.allCategories.data} />
+        <AddCourseForm categories={loaderData.sortedCategories} />
       </div>
       <div className="flex flex-col gap-10 p-5 bg-darker rounded-md">
         <span className="text-xl font-lalezar text-white">
