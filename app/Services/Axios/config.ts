0e6c1eac9f9a3a76f4708b76fca53b7055c96709
Alireza <alirezaaa1194@gmail.com>
2025-03-11 02:46:07 +0330

    redeploy by vercel backend

--- app/Services/Axios/config.ts
+++ app/Services/Axios/config.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const apiRequest = axios.create({
-  // baseURL: "https://react-sabzlearn.liara.run/v1",
-  baseURL: "http://127.0.0.1:4000/v1",
+  baseURL: "https://sabzlearn-backend-ochre.vercel.app/v1",
+  // baseURL: "http://127.0.0.1:4000/v1",
   headers: {
     "Content-Type": "application/json",
   },
